f850868dc29fdaef2a6f8d3cad49f9c71e3038c5
AlanSantana98 <alansantana1498@gmail.com>
2025-01-21 14:40:53 -0300

    Adicionar arquivos de projeto.

---
+++ Target_Sistemas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Target_Sistemas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
